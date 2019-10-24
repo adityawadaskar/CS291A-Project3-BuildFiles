1bbce6094abaf9e9de06349c73b981c55b808f5b
Aditya Wadaskar <wadaskar.aditya@gmail.com>
2019-10-24 16:52:41 -0700

    Add build files

---
+++ build/precache-manifest.435c4fe4e7d330c6e44d87eea46ba97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2bc8144fe7ad67527666e1645fcb24",
+    "url": "/chat_client/index.html"
+  },
+  {
+    "revision": "929a119ec2ff942517dc",
+    "url": "/chat_client/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c394eddf856714b67213",
+    "url": "/chat_client/static/css/main.5ae214e9.chunk.css"
+  },
+  {
+    "revision": "929a119ec2ff942517dc",
+    "url": "/chat_client/static/js/2.20f16481.chunk.js"
+  },
+  {
+    "revision": "c394eddf856714b67213",
+    "url": "/chat_client/static/js/main.1ea41f72.chunk.js"
+  },
+  {
+    "revision": "910f7eec4a4f1497b4e5",
+    "url": "/chat_client/static/js/runtime-main.af15725c.js"
+  }
+]);
